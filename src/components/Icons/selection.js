19a84b28433f72c3dccd9c6c9374a64f5521d1cf
Zihao Zhai <zihao@evestemptation.com>
2018-08-14 11:10:50 -0700

    pull test

--- src/components/Icons/selection.js
+++ src/components/Icons/selection.js
@@ -1,5 +1,6 @@
 // this is the selection.json file from IcoMoon, with "export default" added at the start and renamed from '.json' to '.js';
 
+
 export default {
   "IcoMoonType": "selection",
   "icons": [
